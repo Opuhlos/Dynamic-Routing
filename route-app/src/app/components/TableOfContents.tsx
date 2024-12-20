639e19868ab51261395dfbf8f5e6a810c22d6ddd
Opuhlos <ericnguyen455@gmail.com>
2024-12-20 15:40:09 -0500

    changed main page styling

--- route-app/src/app/components/TableOfContents.tsx
+++ route-app/src/app/components/TableOfContents.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 export default function TableOfContents() {
 
     return(
-        <div className="bg-black text-white w-40 rounded-md">
-            <h1 className="text-center text-lg">Table of Contents</h1>
+        <div className="bg-black text-white">
+            <h1 className="text-center text-4xl py-3">Table of Contents</h1>
 
             <div className="flex flex-col">
                 {games.map((route) => (
-                    <Link href={`/games/${route.id}/${route.seo_name}`} key={route.id}> {route.name} </Link>
+                    <Link className="pl-3 py-3 text-2xl hover:bg-slate-900" href={`/games/${route.id}/${route.seo_name}`} key={route.id}> {route.name} </Link>
                 ))}
             </div>
 
